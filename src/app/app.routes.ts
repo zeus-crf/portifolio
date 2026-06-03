0db801ea987c84919c61f25d03b107c941da8b13
Miguel Lopes <mpmiguel2007@gmail.com>
2026-06-03 20:43:36 -0300

    feat: Criação das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+    {path: 'home', loadComponent: () => import('./features/home/home.component')
+        .then(m => m.HomeComponent),
+        data: {title: 'Home - Portifólio'}
+    },
+
+    {path: 'about', loadComponent: () => import('./features/about/about.component')
+        .then(m => m.AboutComponent),
+        data: {title: 'Sobre - Portifólio'}
+    },
+
+    {path: 'projects', loadComponent: () => import('./features/projects/projects.component')
+        .then(m => m.ProjectsComponent),
+        data: {title: 'Projetos - Portifólio'}
+    },
+
+    {path: 'contact', loadComponent: () => import('./features/contact/contact.component')
+        .then(m => m.ContactComponent),
+        data: {title: 'Contato - Portifólio'}
+    },
+
+    {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
